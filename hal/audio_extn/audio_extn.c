147742f9af69aa4e7aba158550e5aebbbbe505cc
ShujathMohd <shujath@outlook.in>
2023-10-23 13:04:33 +0530

    hal: audio_extn: fix launch_power_policy method call
    vendor/qcom/opensource/audio-hal/primary-hal/hal/audio_extn/audio_extn.c:6604:24: error: passing arguments to a function without a prototype is deprecated in all versions of C and is not supported in C2x [-Werror,-Wdeprecated-non-prototyp
    e]
        launch_power_policy(init_config);
                           ^
    aurguement is not required.

--- hal/audio_extn/audio_extn.c
+++ hal/audio_extn/audio_extn.c
@@ -6601,7 +6601,7 @@ static void* power_policy_thread_func(void* arg __unused) {
     power_policy_init_config_t init_config;
     init_config.fp_in_set_power_policy = in_set_power_policy;
     init_config.fp_out_set_power_policy = out_set_power_policy;
-    launch_power_policy(init_config);
+    launch_power_policy();
 
 exit:
     return NULL;
